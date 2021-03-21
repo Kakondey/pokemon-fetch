b9eed4044326001701f50225c435ae5adbbc0042
kakon dey <kakondey701@gmail.com>
2021-03-21 20:58:02 +0530

    added pagination

--- src/Components/ListPokemon.js
+++ src/Components/ListPokemon.js
@@ -3,25 +3,51 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 const ListPokemon = () => {
   const [pokeList, setPokeList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pokemonPerPage] = useState(10);
+
   useEffect(() => {
     axios
-      .get(
-        "https://pokeapi.co/api/v2/pokemon?offset=$%7Boffset%7D&limit=$%7Blimit%7D"
-      )
+      .get("https://pokeapi.co/api/v2/pokemon?offset=100&limit=1038")
       .then((response) => {
         // console.log();
         setPokeList(response.data.results);
       })
       .catch((error) => {});
   }, []);
+  console.log(pokeList);
+  //   pagination logic
+  //   const totalPages = Math.ceil(pokeList.length / pokemonPerPage);
+  const indexOfLastpokemon = currentPage * pokemonPerPage;
+  const indexOfFirstpokemon = indexOfLastpokemon - pokemonPerPage;
+  const currentPokemons = pokeList.slice(
+    indexOfFirstpokemon,
+    indexOfLastpokemon
+  );
 
-  var str = "https://pokeapi.co/api/v2/pokemon/2/";
+  const paginationArray = [];
+  for (let i = 1; i <= Math.ceil(pokeList.length / pokemonPerPage); ++i) {
+    paginationArray.push(i);
+  }
 
-  var x = str.match("[0-9]+(?=/$)")[0];
-  console.log(x);
+  const handlePaginate = (index) => {
+    setCurrentPage(index);
+  };
+
+  const handleNext = () => {
+    setCurrentPage(
+      currentPage === pokeList.length / pokemonPerPage ? 1 : currentPage + 1
+    );
+  };
+  const handlePrev = () => {
+    setCurrentPage(
+      currentPage === 1 ? pokeList.length / pokemonPerPage - 1 : currentPage - 1
+    );
+  };
+
   return (
     <div>
-      {pokeList.map((pokemon) => {
+      {currentPokemons.map((pokemon) => {
         return (
           <Link
             key={pokemon.url.match("[0-9]+(?=/$)")[0]}
@@ -33,6 +59,17 @@ const ListPokemon = () => {
           </Link>
         );
       })}
+      <div>
+        <button onClick={handlePrev}>Prev</button>
+        <button onClick={handleNext}>Next</button>
+        {paginationArray.map((index) => {
+          return (
+            <p onClick={() => handlePaginate(index)} key={index}>
+              {index}
+            </p>
+          );
+        })}
+      </div>
     </div>
   );
 };
